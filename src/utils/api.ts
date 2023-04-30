b76b922ded68d704770964e4b1d039637cd85517
Axel Asp <solidtop.asp@gmail.com>
2023-04-30 17:40:42 +0200

    Remove code

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,15 +1,9 @@
-import { Movie, Screening } from "@/types";
+import { Movie } from "@/types";
 
 const API_URL: string = "https://plankton-app-xhkom.ondigitalocean.app/api";
 
 export async function getMovies(): Promise<Movie[]> {
   const res = await fetch(API_URL + "/movies");
-  const payload = await res.json();
-  return payload.data;
-}
-
-export async function getScreenings(): Promise<Screening[]> {
-  const res = await fetch(API_URL + "/screenings?populate=movie");
   const payload = await res.json();
   return payload.data;
 }
