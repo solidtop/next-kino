e2468001622d939cd7ba9364c4a2c3cabc70ba60
Anton Holst <holst9559@hotmail.com>
2023-05-09 15:36:16 +0200

    minor changes

--- src/app/api/seating/route.ts
+++ src/app/api/seating/route.ts
@@ -3,38 +3,24 @@ import TicketsModel from "@/models/tickets";
 import connectTicketDb from "@/utils/connectTicketDb";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  connectTicketDb();
-
-  const screeningId = req.nextUrl.searchParams.values().next().value;
-
-  const screening: Array<any> = await TicketsModel.find({
-    screeningId: screeningId,
-  });
-
-  const seating: Array<number> = [];
-  screening.forEach((s) => {
-    s.seats.forEach((num: number) => {
-      seating.push(num);
-    });
-  });
-
-  return NextResponse.json(seating);
-}
-
-/*
-async function handleTickets(booking: object) {
   try {
-    const res = await fetch("/api/seating?screeningId={*INSERT SCREENING ID HERE*}", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+    connectTicketDb();
+    const screeningId = req.nextUrl.searchParams.values().next().value;
+
+    const screening: Array<any> = await TicketsModel.find({
+      screeningId: screeningId,
     });
 
-    const payload = await res.json();
+    const seating: Array<number> = [];
+
+    screening.forEach((s) => {
+      s.seats.forEach((num: number) => {
+        seating.push(num);
+      });
+    });
+
+    return NextResponse.json(seating);
   } catch (err) {
     console.log(err);
   }
 }
-
-*/
