0bdcfacf2f2c432c310be5be62e461a72fd655fc
Anton Holst <holst9559@hotmail.com>
2023-05-09 12:17:41 +0200

    Used existing ticket type and added movie title to database

--- src/models/tickets.ts
+++ src/models/tickets.ts
@@ -1,21 +1,15 @@
-import { Schema, model, Document } from "mongoose";
-
-interface iTickets extends Document {
-  id: Number;
-  type: String;
-  price: Number;
-  quantity: Number;
-  maxQuantity: Number;
-}
+import { Schema, model } from "mongoose";
+import { Ticket } from "@/types";
 
 const TicketsSchema: Schema = new Schema({
   bookingId: Number,
   screeningId: Number,
+  movie: String,
   startTime: String,
-  tickets: Array<iTickets>,
+  tickets: Array<Ticket>,
   seats: Array<Number>,
   email: String,
 });
 
-const TicketsModel = model<iTickets>("Ticket", TicketsSchema);
+const TicketsModel = model<Ticket>("Ticket", TicketsSchema);
 export default TicketsModel;
