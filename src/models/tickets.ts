c46a17649b4bc58cca09d20784e90d9a18182664
Anton Holst <holst9559@hotmail.com>
2023-05-09 11:17:22 +0200

    Created Schema for tickets and a database connection

---
+++ src/models/tickets.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from "mongoose";
+
+interface iTickets extends Document {
+  id: Number;
+  type: String;
+  price: Number;
+  quantity: Number;
+  maxQuantity: Number;
+}
+
+const TicketsSchema: Schema = new Schema({
+  bookingId: Number,
+  screeningId: Number,
+  startTime: String,
+  tickets: Array<iTickets>,
+  email: String,
+});
+
+const TicketsModel = model<iTickets>("Ticket", TicketsSchema);
+export default TicketsModel;
