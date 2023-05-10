8e65ef89cd8be47b2f422b2323d5a4ef4a7f3a70
Anton Holst <holst9559@hotmail.com>
2023-05-10 10:56:39 +0200

    Function to create seating array

---
+++ src/utils/seatingTemplate.ts
@@ -0,0 +1,8 @@
+export default function initTheater() {
+  const seats: number = 80;
+  const seatingArray: Array<number> = [];
+
+  for (let i = 0; i < seats; i++) {
+    seatingArray.push(i + 1);
+  }
+}
