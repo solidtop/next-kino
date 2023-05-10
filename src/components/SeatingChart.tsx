cbf3a5c97ca7965d80885233b43daac3d2d8a60b
Anton Holst <holst9559@hotmail.com>
2023-05-10 12:11:00 +0200

    Seating chart completed, visually

--- src/components/SeatingChart.tsx
+++ src/components/SeatingChart.tsx
@@ -3,7 +3,7 @@ import SeatingGrid from "./SeatingGrid";
 import initTheater from "@/utils/seatingTemplate";
 
 const SeatingChart: FC<any> = ({ bookingDetails }) => {
-  const seating = initTheater(80);
+  const seating = initTheater(96);
   return (
     <div className="p-4 bg-container-color rounded">
       <div className="font-semibold bg-gray-950 text-center text-3xl pt-1 h-12 w-96 ml-auto mr-auto rounded">
--- src/components/SeatingGrid.tsx
+++ src/components/SeatingGrid.tsx
@@ -2,10 +2,15 @@ import { FC } from "react";
 
 const SeatingGrid: FC<any> = ({ seating }) => {
   return (
-    <div className="min-w-40">
+    <div className="flex flex-wrap flex-row justify-center gap-1.5 pt-16 mb-2">
       {seating.map((seat: number) => {
         const seatIndex: number = seat;
-        return <div className="bg-bg-color h-6 w-6 rounded"></div>;
+        return (
+          <div
+            key={seatIndex}
+            id={seatIndex.toString()}
+            className="bg-gray-700 h-8 w-8 rounded hover:bg-gray-400 hover:cursor-pointer"></div>
+        );
       })}
     </div>
   );
