a1f3afbab5ee0021603ff4c78c817c651a76fc04
3xer <pontuslundholm1@gmail.com>
2023-05-11 15:38:18 +0200

    for branch switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import MovieList from "@/components/MovieList";
 import Header from "@/components/Header";
 import * as api from "@/utils/api";
 
+//temp
+import MovieInfo from '@/components/MovieInfo'
+
 export default async function Home() {
   const movies = await api.getMovies();
   const loggedIn: boolean = false;
@@ -9,6 +12,7 @@ export default async function Home() {
     <main>
       <Header loggedIn={loggedIn} />
       {movies && <MovieList movies={movies}></MovieList>}
+      <MovieInfo/>
     </main>
   );
 }
