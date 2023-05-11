bc82b2c5277ce437017ae5220e7a26acb629e9d9
Anton Holst <holst9559@hotmail.com>
2023-05-11 10:47:26 +0200

    More visual display for selected seats

--- src/components/SeatingLegend.tsx
+++ src/components/SeatingLegend.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const SeatingLegend: FC<any> = () => {
   return (
-    <div className="p-4 mt-1 bg-container-color rounded font-semibold ">
+    <legend className="p-4 mt-1 bg-container-color rounded font-semibold ">
       Om färgerna i stolplanen:
       <div className="flex justify-center gap-1 mt-4">
         <div className="h-6 w-6 bg-available rounded" id="available"></div>
@@ -16,7 +16,7 @@ const SeatingLegend: FC<any> = () => {
         <div id="occupied" className="h-6 w-6 bg-occupied rounded ml-10"></div>
         <label htmlFor="occupied">Upptagen</label>
       </div>
-    </div>
+    </legend>
   );
 };
 
--- src/components/SelectedSeats.tsx
+++ src/components/SelectedSeats.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
 import SeatIndication from "./SeatIndication";
 
-const SelectedSeats: FC<any> = ({ bookingDetails }) => {
+const SelectedSeats: FC<any> = ({ bookingDetails, selectedSeats }) => {
   const seatArray: Array<number> = bookingDetails.seats;
-  return (
-    <div className="flex flex-row ml-11 mt-12 text-xl">
-      Valda platser:
-      {seatArray.map((seat) => (
-        <SeatIndication seat={seat} />
-      ))}
-    </div>
-  );
+
+  if (selectedSeats < seatArray.length) {
+    return (
+      <div className="flex flex-row ml-11 mt-12 text-xl">Valda platser:</div>
+    );
+  } else {
+    return (
+      <div className="flex flex-row ml-11 mt-12 text-xl">
+        Valda platser:
+        {seatArray.map((seat) => (
+          <SeatIndication seat={seat} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default SelectedSeats;
