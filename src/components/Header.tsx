82ac60b04be9b3e44c3e9acc870eeb361ea20c4d
Anton Holst <holst9559@hotmail.com>
2023-05-12 08:52:45 +0200

    Fixed logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import Link from "next/link";
 import LoginModal from "./LoginModal";
 import MyPages from "./MyPages";
+import Image from "next/image";
+import logo from "../../public/icons/Logo.png";
 
 //Still temporary code, will swap loggedIn for props from something like next-auth.
 //Probably with useSession to get session data
@@ -10,7 +12,7 @@ const Header: FC<{ loggedIn: boolean }> = (loggedIn) => {
     <section>
       <header className="grid grid-cols-5 mt-6 ">
         <Link className="order-first justify-start ml-10" href="/">
-          <img src="_next/static/public/Logo.png" alt="Spegeln Logo" />
+          <Image src={logo} alt="Spegeln Logo" />
         </Link>
 
         <ul className="flex flex-row col-start-2 col-end-5 text-xl font-semibold justify-center mt-6 gap-14">
