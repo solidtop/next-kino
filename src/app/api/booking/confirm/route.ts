782439722b34aad89c21f58054f58140db7fc915
Anton Holst <holst9559@hotmail.com>
2023-05-12 19:42:46 +0200

    Posts tickets to database at confirmation

--- src/app/api/booking/confirm/route.ts
+++ src/app/api/booking/confirm/route.ts
@@ -7,6 +7,9 @@ import {
   getBookingSession,
   loadBookingSession,
 } from "@/utils/bookingSession";
+import connectdb from "@/utils/connectDb";
+import TicketsModel from "@/models/tickets";
+import { Ticket } from "@/types";
 
 export async function POST() {
   const session = getBookingSession();
@@ -20,6 +23,31 @@ export async function POST() {
   }
 
   /* Save to database */
+
+  const bookingId = bookingDetails.id;
+  const screeningId = bookingDetails.screening.id;
+  const movie = bookingDetails.screening.attributes.movie.data.attributes.title;
+  const startTime = bookingDetails.screening.attributes.start_time;
+  const tickets = bookingDetails.tickets;
+  const seats = bookingDetails.seats;
+  const email = bookingDetails.email;
+
+  try {
+    connectdb();
+    const ticketsDb = new TicketsModel({
+      bookingId,
+      screeningId,
+      movie,
+      startTime,
+      tickets,
+      seats,
+      email,
+    });
+
+    await ticketsDb.save();
+  } catch (err) {
+    return errorResponse("Det gick inte att bekräfta er bokning", 401);
+  }
 
   const res = NextResponse.json(bookingDetails);
   endBookingSession(res);
