2189f2c4ac49b941ea099a1bad465916c1570b61
Anton Holst <holst9559@hotmail.com>
2023-05-12 12:38:30 +0200

    Db fixes

--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGO_URI || "";
 
 export default function connectdb() {
-  mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-      console.log("Database connection successful");
-    })
-    .catch((error) => {
-      console.log("Database connection error:", error);
-    });
+  if (mongoose.connection.readyState === 0) {
+    mongoose
+      .connect(MONGODB_URI)
+      .then(() => {
+        console.log("Database connection successful");
+      })
+      .catch((error) => {
+        console.log("Database connection error:", error);
+      });
+  }
 }
