a22d065c0967901afc84b057d3d7b4515fe223c6
Anton Holst <holst9559@hotmail.com>
2023-05-12 12:31:47 +0200

    Fixed mongoDB connections

--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGO_URI || "";
 
 export default function connectdb() {
-  mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-      console.log("Database connection successful");
-    })
-    .catch((error) => {
-      console.log("Database connection error:", error);
-    });
+  //Once conneciton is made, prevents further tries at connecting
+  if (mongoose.connection.readyState === 0) {
+    mongoose
+      .connect(MONGODB_URI)
+      .then(() => {
+        console.log("Database connection successful");
+      })
+      .catch((error) => {
+        console.log("Database connection error:", error);
+      });
+  }
 }
