2b69c4a8bf3b3d1a1c0525d80a922dce4614e2d7
Anton Holst <holst9559@hotmail.com>
2023-05-15 15:52:07 +0200

    Started working on userPage

--- src/app/myPages/page.tsx
+++ src/app/myPages/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import Header from "@/components/Header";
+import UserInformation from "@/components/UserInformation";
+import { useEffect, useState } from "react";
 
 export default function MyPages() {
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    setCurrentUser(user);
+  }, []);
+
   return (
     <>
       <Header />
-      <div>MY PAGES</div>
+      <UserInformation currentUser={currentUser} />
     </>
   );
 }
---
+++ src/components/UserInformation.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const UserInformation: FC<any> = (currentUser) => {
+  const user = currentUser.currentUser;
+
+  return (
+    <div>
+      <h1>Nothing Here</h1>
+    </div>
+  );
+};
+
+export default UserInformation;
