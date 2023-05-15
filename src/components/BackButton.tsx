f74c428780cc07bfbfb077418e3c584b132ecbec
Axel Asp <solidtop.asp@gmail.com>
2023-05-15 14:09:25 +0200

    Change images to <Image>

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { FC } from "react";
+import Image from "next/image";
+import ChevronIcon from "../../public/icons/chevron-left-solid.svg";
 
 const BackButton: FC = () => {
   return (
@@ -9,11 +11,7 @@ const BackButton: FC = () => {
         onClick={() => history.go(-1)}
         className="lg:absolute top-4 right-3/4 inline-flex items-center h-8 px-4 rounded lg:mx-10 bg-btn-primary-color hover:brightness-110"
       >
-        <img
-          src="/icons/chevron-left-solid.svg"
-          alt="chevron left"
-          className="w-2 mr-2"
-        />
+        <Image src={ChevronIcon} alt="chevron left" className="w-2 mr-2" />
         Tillbaka
       </Link>
     </div>
