c2166c4ab031d6d8f96b0c6be6523d883e43c52d
Anton Holst <holst9559@hotmail.com>
2023-05-15 15:14:49 +0200

    Created a myPages site

---
+++ src/app/myPages/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function MyPages() {
+  return (
+    <>
+      <Header />
+      <div>MY PAGES</div>
+    </>
+  );
+}
--- src/components/MyPages.tsx
+++ src/components/MyPages.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from "react";
 import { useRouter } from "next/navigation";
 import { RiArrowDownSLine } from "react-icons/ri";
+import Link from "next/link";
 
 interface MyPagesProps {
   handleLogout: () => void;
@@ -22,24 +23,21 @@ const MyPages: FC<MyPagesProps> = ({ handleLogout }) => {
     <div className="relative z-10">
       <button
         className="order-last col-start-5 flex justify-end mr-10 mt-6 text-xl font-semibold"
-        onClick={toggleDropdown}
-      >
+        onClick={toggleDropdown}>
         {user.name} <RiArrowDownSLine className="ml-1 mt-1" />
       </button>
       {isOpen && (
         <div className="absolute right-30 mt-2 bg-container-color rounded shadow-md">
           {user.name && (
-            <a
-              href="/"
-              className="block px-4 py-2  text-white-800 rounded  hover:bg-white hover:bg-opacity-10 text-left"
-            >
+            <Link
+              href={"/myPages"}
+              className="block px-4 py-2  text-white-800 rounded  hover:bg-white hover:bg-opacity-10 text-left">
               Mina sidor
-            </a>
+            </Link>
           )}
           <button
             className="block px-4 py-2 w-full text-white-800 rounded hover:bg-white hover:bg-opacity-10 text-left"
-            onClick={handleLogout}
-          >
+            onClick={handleLogout}>
             Logga ut
           </button>
         </div>
