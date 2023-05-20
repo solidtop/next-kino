a57ebdabad52012f6efa3e85710873ba40765c97
3xer <pontuslundholm1@gmail.com>
2023-05-20 13:10:10 +0200

    added footer

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,20 +1,23 @@
 import { getMovie, getScreeningById } from "@/utils/api";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import MovieDetails from "@/components/MovieDetails";
 import Screenings from "@/components/Screenings";
 
-export default async function DetailsPage({ params }: { params: { id: string }}) {
+export default async function DetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const details = await getMovie(params.id);
-  const screenings = await getScreeningById(params.id)
-  
-  
+  const screenings = await getScreeningById(params.id);
+
   return (
     <>
       <Header />
       <MovieDetails movieDetails={details} />
       <Screenings screenings={screenings} />
+      <Footer/>
     </>
   );
 }
-
-
