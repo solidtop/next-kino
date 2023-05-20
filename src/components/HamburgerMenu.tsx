ef2d65e808abaf7ffda501b5a5143fc588bdc7a9
Anton Holst <holst9559@hotmail.com>
2023-05-20 14:40:28 +0200

    Completed burger menu

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,55 +1,69 @@
-import { FC, useState, useEffect } from "react";
-import { stack as Menu } from "react-burger-menu";
+import { FC, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Modal from "./Modal";
+import hamburgerIcon from "../../public/icons/hamburger-icon.png";
+import closeIcon from "../../public/icons/icons8-close-50.png";
+import logo from "../../public/icons/biospegeln.png";
 
 type HamburgerMenuProps = {
   showModal: boolean;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const HamburgerIcon = () => (
-  <div className="flex lg:hidden relative w-12">
-    <svg
-      className="w-12 h-12 text-white"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-      stroke="currentColor">
-      <path d="M4 6h16M4 12h16M4 18h16"></path>
-    </svg>
-  </div>
-);
-
 const HamburgerMenu: FC<HamburgerMenuProps> = ({ showModal, setShowModal }) => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
   return (
-    <div className="relative p-2 lg:hidden">
-      <Menu
-        noOverlay
-        width={"100%"}
-        customBurgerIcon={<HamburgerIcon />}
-        className="left-0 top-0 bg-container-color p-10 rounded">
-        <ul className="flex flex-col">
-          <button
-            className="flex lg:hidden text-xl font-semibold"
-            onClick={() => setShowModal(true)}>
-            Login/Register
-          </button>
-          {showModal && <Modal setShowModal={setShowModal} />}
-          <li>
-            <Link href="/information/contact">Öppettider & Kontakt</Link>
-          </li>
-          <li>
-            <Link href="/information/about">Om Spegeln</Link>
-          </li>
-          <li>
-            <Link href="/information/tickets">Biljettinfo</Link>
-          </li>
-        </ul>
-      </Menu>
-    </div>
+    <>
+      <Image
+        src={hamburgerIcon}
+        alt="Menu icon"
+        width={50}
+        height={50}
+        onClick={() => setMenuOpen(true)}
+        className="hover:cursor-pointer lg:hidden mr-2"
+      />
+      {menuOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-bg-color p-8 w-full h-full relative">
+            <Image
+              src={logo}
+              alt="Spegeln Logo"
+              className="w-24 absolute top-4 left-4"
+            />
+            <Image
+              src={closeIcon}
+              alt="Close icon"
+              height={45}
+              width={45}
+              color="white"
+              className="absolute right-6 top-8 hover:cursor-pointer"
+              onClick={() => setMenuOpen(false)}
+            />
+            <ul className="flex flex-col gap-10 mt-20">
+              <li>
+                <button
+                  className="text-2xl font-semibold"
+                  onClick={() => setShowModal(true)}>
+                  Login/Register
+                </button>
+              </li>
+              {showModal && <Modal setShowModal={setShowModal} />}
+              <li>
+                <Link href="/information/contact">Öppettider & Kontakt</Link>
+              </li>
+              <li>
+                <Link href="/information/about">Om Spegeln</Link>
+              </li>
+              <li>
+                <Link href="/information/tickets">Biljettinfo</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
