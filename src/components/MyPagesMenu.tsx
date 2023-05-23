6f156976dbc90849dacb9b95dd81d96e697ecb94
Axel Asp <solidtop.asp@gmail.com>
2023-05-23 12:44:06 +0200

    Add missing comp

---
+++ src/components/MyPagesMenu.tsx
@@ -0,0 +1,52 @@
+import React, { FC } from "react";
+import { RiArrowDownSLine } from "react-icons/ri";
+import Image from "next/image";
+import userIcon from "/public/icons/user-solid.svg";
+import Link from "next/link";
+import { User } from "@/types";
+
+interface MyPagesMenuProps {
+  handleLogout: () => void;
+  user: User;
+  isOpen: boolean;
+  toggleDropdown: () => void;
+}
+
+const MyPagesMenu: FC<MyPagesMenuProps> = ({
+  handleLogout,
+  user,
+  toggleDropdown,
+  isOpen,
+}) => {
+  return (
+    <div className="relative z-10">
+      <div className="flex items-center gap-2">
+        <Image src={userIcon} alt="user icon" width={16} />
+        <button
+          className="flex justify-end text-lg font-semibold"
+          onClick={toggleDropdown}
+        >
+          {user.name} <RiArrowDownSLine className="ml-1 mt-1" />
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute right-30 mt-2 bg-container-color rounded shadow-md">
+          <Link
+            href={"/my-pages"}
+            className="block px-4 py-2  text-white-800 rounded  hover:bg-white hover:bg-opacity-10 text-left"
+          >
+            Mina sidor
+          </Link>
+          <button
+            className="block px-4 py-2 w-full text-white-800 rounded hover:bg-white hover:bg-opacity-10 text-left"
+            onClick={handleLogout}
+          >
+            Logga ut
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyPagesMenu;
